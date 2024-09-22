1e0e18920a3aa2613d0ffb94ba005c433aa11c6c
George <georgewagdy123@gmail.com>
2024-09-21 22:28:57 -0400

    Used urllib to download the dataset in input

--- Language Model - Andrej/main/main.py
+++ Language Model - Andrej/main/main.py
@@ -1,1 +1,8 @@
-print("hello")
+import urllib.request as ur
+
+url = r"https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt"
+path = r"C:\Users\georg\OneDrive\Desktop\Projects\Language Model - Andrej\Creating-A-Transformer\Language Model - Andrej\\input"
+
+ur.urlretrieve(url, path)
+
+#getting the dataset
